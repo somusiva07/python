bfb38c2c253336275483bf8afcb43442a8a0d000
somusiva07 <somusiva07@gmail.com>
2026-02-28 21:48:57 +0530

    User Input example
    User Input example

---
+++ starters/userInput.py
@@ -0,0 +1,11 @@
+print('Enter your name')
+name = input()
+print(f'Hello {name}')
+
+age = input("Enter your Age:")
+print(f"Your age is {age}")
+
+fav1 = input("What is your favorite animal:")
+fav2 = input("What is your favorite color:")
+fav3 = input("What is your favorite number:")
+print(f"Do you want a {fav2} {fav1} with {fav3} legs?")
